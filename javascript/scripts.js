f013b016f1d42dfb2af131b19fb13e63f0d41078
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-04 23:52:32 +0300

    started addiing my js code

---
+++ javascript/scripts.js
@@ -0,0 +1,4 @@
+let akan = function( date of birth){
+    let femaleNames = ["Akosoa", "adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", "Kudwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+}
