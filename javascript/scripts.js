797633c4172bbe05be359722207efa7a83e8a8ee
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-06 16:12:28 +0300

    editted and added to js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,45 @@
- let akanNames = function(dateOfBirth,gender){return akanNames}
-let dateOfBirth = function("MM","DD","YY"){return "MM" + "/" + "DD" + "/" + "YY"}
- let dateOfBirth = prompt("enter date");
- let gender = prompt("enter gender");
+ document.getElementById("button").onclick = function(yourAkanName){
+     let date = document.getElementById("input1").value;
+     let gender = document.getElementsByName("gender").value;
+     
+     function getGender() {
+         for (let gender of genders){
+             if(gender.checked){
+                 return gender.value
+             }
+         }
+     }
+     let myGenderValue =getGender();
+     alert(myGenderValue);
+     
+     function dateValidator() {
+         if (monthOfBirth === && Number(yearOfBirth)%4 === 0){
+             if (dayOfBirth > 28 or dayOfBirth < 1 ){
+                 return false;}
 
+                 else if (monthOfBirth === 2 && dayOfBirth >29){
+                     return false;}
+
+                     else if (monthOfBirth === 2 && dayOfBirth < 1){
+                         return false;}
+
+                         else {
+                             return true;
+                         }
+                        
+                         let monthValid = monthValidator();
+                         let dayValid = dayValidator();
+
+                         let dayOfweekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+((5*Number(yearOfBirth.slice(2,4))/4))((26*(monthOfBirth+1)/10))+dayOfBirth)%7)
+                         let maleAkanNames = ["kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+                         let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+                     
+
+
+                 
+                 
+
+             
+         }
+     }
+ }
