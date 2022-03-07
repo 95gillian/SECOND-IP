05324fb93dab512a2c8d31199d3582a97034a6f9
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-07 22:28:17 +0300

    new commit for js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,45 +1,25 @@
- document.getElementById("button").onclick = function(yourAkanName){
-     let date = document.getElementById("input1").value;
-     let gender = document.getElementsByName("gender").value;
-     
-     function getGender() {
-         for (let gender of genders){
-             if(gender.checked){
-                 return gender.value
-             }
-         }
-     }
-     let myGenderValue =getGender();
-     alert(myGenderValue);
-     
-     function dateValidator() {
-         if (monthOfBirth === && Number(yearOfBirth)%4 === 0){
-             if (dayOfBirth > 28 or dayOfBirth < 1 ){
-                 return false;}
 
-                 else if (monthOfBirth === 2 && dayOfBirth >29){
-                     return false;}
-
-                     else if (monthOfBirth === 2 && dayOfBirth < 1){
-                         return false;}
-
-                         else {
-                             return true;
-                         }
-                        
-                         let monthValid = monthValidator();
-                         let dayValid = dayValidator();
-
-                         let dayOfweekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+((5*Number(yearOfBirth.slice(2,4))/4))((26*(monthOfBirth+1)/10))+dayOfBirth)%7)
-                         let maleAkanNames = ["kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-                         let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-                     
+    let date = document.getElementById("date").value;
+    let month = document.getElementById("month");
+    let year = document.getElementById("year")
+    let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwahu","Yaw","Kofi","Kwame"];
+    let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    let dayOfWeek = ["Monday", "Tuesday",  "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let genders = document.getElementByName("gender");
 
 
-                 
-                 
 
-             
-         }
-     }
- }
+
+
+
+
+
+
+
+
+
+   
+
+       
+
+       
