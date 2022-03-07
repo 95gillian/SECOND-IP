97cc8e3d55c2ad28ef7e9a3d24b682f5019da11f
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-07 23:17:31 +0300

    edited style.css

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,27 @@
 
-    let date = document.getElementById("date").value;
-    let month = document.getElementById("month");
-    let year = document.getElementById("year")
+    let date = document.getElementById("input1");
+    let month = document.getElementById("input2");
+    let year = document.getElementById("input3");
     let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwahu","Yaw","Kofi","Kwame"];
     let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     let dayOfWeek = ["Monday", "Tuesday",  "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    let genders = document.getElementByName("gender");
+    let genders = document.getElementById("gender");
+
+    let gender =["male","female"]
+
+    function myFunction()) {
+        let tarehe = new date();
+
+       
+     if(date.value === '' || date.value == null && month.value === '' || month.value == null && year.value === '' || year.value == null){
+            if(date.value == null || month.value == null || year.value == null){
+                alert('please input date')
+            }
+            alert('please input date')
+        }
+        
+    }
+    }
 
 
 
